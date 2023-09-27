2410633af4b9dee591e82204973cc2fe4152f2d8
Sean <seanmarman@gmail.com>
2023-09-26 19:18:00 -0700

    allow branch protection rule

--- configureRepo/configureRepo.go
+++ configureRepo/configureRepo.go
@@ -1,11 +1,48 @@
 package configureRepo
 
 import (
+	"context"
 	"fmt"
+	"os"
 
-	"github.com/smarman85/gitTemplate/newFromTemplate"
+	"github.com/google/go-github/v55/github"
 )
 
-func Configure(r newFromTemplate.Request {
-	fmt.Printf(r)
-})
+type repo interface {
+	setBranchProtection()
+}
+
+type TargetRepo struct {
+	Repo, Owner string
+	client      *github.Client
+	ctx         context.Context
+}
+
+func (t TargetRepo) setBranchProtection() {
+	rule := github.ProtectionRequest{
+		// RequiredStatusChecks: "",
+		RequiredPullRequestReviews: &github.PullRequestReviewsEnforcementRequest{
+			RequireCodeOwnerReviews: true,
+		},
+		EnforceAdmins: false,
+		// Restrictions: "",
+
+	}
+	p, r, err := t.client.Repositories.UpdateBranchProtection(t.ctx, t.Owner, t.Repo, "main", &rule)
+	if err != nil {
+		fmt.Errorf("error setting branch protection rule: %v", err)
+	}
+
+	fmt.Println(p)
+	fmt.Println(r)
+}
+
+func run(r repo) {
+	r.setBranchProtection()
+}
+
+func Configure(t TargetRepo) {
+	t.client = github.NewClient(nil).WithAuthToken(os.Getenv("GIT_CLONE"))
+	t.ctx = context.Background()
+	run(t)
+}
--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	// "github.com/smarman85/gitTemplate/newFromTemplate"
 	"github.com/smarman85/gitTemplate/configureRepo"
-	"github.com/smarman85/gitTemplate/newFromTemplate"
+	_ "github.com/smarman85/gitTemplate/newFromTemplate"
 )
 
-func Configure() {
-	r := newFromTemplate.Request{
-		NewRepo:         "newRepoFromTemplate",
-		Tempalte:        "template-repo",
-		Owner:           "smarman85",
-		Private:         false,
-		IncludeBranches: false,
+func main() {
+	/*
+		r := newFromTemplate.Request{
+			NewRepo:         "newRepoFromTemplate",
+			Tempalte:        "template-repo",
+			Owner:           "smarman85",
+			Private:         false,
+			IncludeBranches: false,
+		}
+		newFromTemplate.Build(r)
+	*/
+
+	c := configureRepo.TargetRepo{
+		Repo:  "newRepoFromTemplate",
+		Owner: "smarman85",
 	}
-	// newFromTemplate.Build(r)
-	configureRepo.Configure(r)
+	configureRepo.Configure(c)
 }
